072222e3246432c4448957caca2a53a330fd0e92
andre220 <andreprofissional1997@gmail.com>
2020-08-20 03:22:58 -0300

    pagina principal com informacoes basicas e pouco estilo

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+
+function setDates()
+{
+    var currentYear = new Date().getFullYear();
+    document.getElementById("year").innerHTML = currentYear;
+    document.getElementById("idade").innerHTML = currentYear - 1997;
+}
